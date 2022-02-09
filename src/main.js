65d61f240fce3533756af1ee9c27b0aff8dc12f6
Hypeism <52870481+Hypeism@users.noreply.github.com>
2022-02-09 16:33:45 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,9 +15,9 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('src/commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+    const command = require(`src/commands/${file}`);
 
     client.commands.set(command.name, command);
 }
