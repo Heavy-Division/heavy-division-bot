4334bae165709d69cc50bb600a7222e08657895b
Hypeism <jhnwllms236@gmail.com>
2022-03-27 18:48:15 -0400

    index fixes, commented out APM metrics server, to be added later

---
+++ src/commands/index.ts
@@ -0,0 +1,20 @@
+import { CommandDefinition } from '../lib/command';
+import Logger from '../lib/logger';
+import { when } from './support/when';
+
+const commands: CommandDefinition[] = [
+    when
+];
+
+const commandsObject: { [k: string]: CommandDefinition } = {};
+
+for (const def of commands) {
+    for (const name of (typeof def.name === 'string' ? [def.name] : def.name)) {
+        if (commandsObject[name]) {
+            Logger.warn(`Duplicate command/alias inserted: ${name}`);
+        }
+        commandsObject[name] = def;
+    }
+}
+
+export default commandsObject;
---
+++ src/commands/support/when.ts
@@ -0,0 +1,21 @@
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
+
+export const when: CommandDefinition = {
+    name: ['when'],
+    description: 'Explain the absence of release dates or ETAs',
+    category: CommandCategory.GENERAL,
+    executor: async (msg) => {
+        const whenEmbed = makeEmbed({
+            title: 'When is there going to be an update?',
+            description: 'Due to the community-driven nature of our projects, we do not enforce delivery timelines on our volunteer developers. '
+                + 'We value high-quality, polished releases over frequent and quickly-released ones, which means some features might take time to land in a publicly-available build. '
+                + 'Rest assured that our developers and contributors are working hard on making every Heavy Division project as bug-free and feature-complete as possible, '
+                + 'but that we are not able to provide release dates or estimates for a certain feature/fix.',
+        });
+
+        await msg.channel.send({ embeds: [whenEmbed] });
+
+    },
+};
