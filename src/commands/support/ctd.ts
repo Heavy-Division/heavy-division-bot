5c207ad2fcba10c94459cedd1538eb38d7fe4255
Hypeism <jhnwllms236@gmail.com>
2022-03-27 22:53:39 -0400

    Rewrite of ctd.support to reference official MSFS forum support

--- src/commands/support/ctd.ts
+++ src/commands/support/ctd.ts
@@ -1,15 +1,23 @@
 import { CommandDefinition } from '../../lib/command';
-import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const ctd: CommandDefinition = {
     name: ['ctd', 'crash'],
-    description: 'Crash to Desktop',
-    category: CommandCategory.SUPPORT,
+    description: 'Provides direction for addressing CTD issues.',
+    category: CommandCategory.B78XH,
     executor: async (msg) => {
         const ctdEmbed = makeEmbed({
-            title: 'Heavy Division Support | Crash To Desktop',
-            description: 'Find a collection of tips to help with crash to desktops from our friends at FlyByWire [here.](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#ctd-crash-to-desktop)',
+            title: 'Heavy Division | Crash to Desktop',
+            description: makeLines([
+                'Please see the official MSFS forums for support addressing CTD issues. **No support will be offered via Discord.** ',
+                '',
+                '[-Basic Troubleshooting](https://flightsimulator.zendesk.com/hc/en-us/articles/4406280399250-All-versions-Crashing-CTDs-issues-Basic-Troubleshooting) ',
+                '',
+                '[-Crashing/CTD issues Checklist](https://flightsimulator.zendesk.com/hc/en-us/articles/360015893879-All-versions-Crashing-CTDs-issues-Checklist) ',
+                '',
+                '[-Crashing/CTD Advanced Troubleshooting](https://flightsimulator.zendesk.com/hc/en-us/articles/4406280653202-All-versions-Crashing-CTDs-issues-Advanced-Troubleshooting) ',
+            ]),
         });
 
         await msg.channel.send({ embeds: [ctdEmbed] });
