f91c33fc6c1e9aac8d3eedf39408aea730fd3573
Hypeism <jhnwllms236@gmail.com>
2022-06-04 10:06:36 -0700

    pull from main, add test command for PR workflow

---
+++ src/commands/utils/testcommand.ts
@@ -0,0 +1,21 @@
+// based off FlyByWire Simulations Discord Bot - https://github.com/flybywiresim/discord-bot
+
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
+
+export const testcommand: CommandDefinition = {
+    name: ['test'],
+    description: 'Provides links to MSFS support for sim issues',
+    category: CommandCategory.UTILS,
+    executor: async (msg) => {
+        const testEmbed = makeEmbed({
+            title: 'Test command',
+            description: 'this is a test command for PR workflows.'
+
+        });
+
+        await msg.channel.send({ embeds: [testEmbed] });
+
+    },
+};
