1c74ffac3e4aba053d835042da8bc3bdd57f6100
Omar <70115175+ko-zone@users.noreply.github.com>
2022-11-26 09:59:51 +0200

    revised text

--- src/commands/support/cycle.ts
+++ src/commands/support/cycle.ts
@@ -15,10 +15,9 @@ export const cycleWarning: CommandDefinition = {
             description: makeLines([
                 'Users will occasionally see "end date of data base has passed select new cycle on ident page."',
 				"",
-				'There are several reasons why this message may appear: ',
+				'There are a couple of reasons why this message may appear: ',
                 '- MSFS navdata is out of date.',
                 '- Your navdata is out of date.',
-                '- Your simBrief selected AIRAC is incorrect/not matching with the simulator.',
 				'',
 				'If you own a navigraph subscription, close the sim and update your navdata using their installer. If you do not own a navigraph subscription, and are facing no issues with flight plan imports, this message can be safely ignored. If you face flight plan import issues when seeing this message, check that you are using the correct AIRAC cycle from your simBrief dispatch.',
             ]),
