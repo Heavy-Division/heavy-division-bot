2e723d22b54a2fffcf10db720392b138087dcd4b
nakajimayoshi <Yoshi@hdsimulations.com>
2024-07-12 21:58:40 +0900

    lint

--- src/commands/memes/cowsay.ts
+++ src/commands/memes/cowsay.ts
@@ -1,26 +1,21 @@
 // based off FlyByWire Simulations Discord Bot - https://github.com/flybywiresim/discord-bot
 
 import { say } from 'cowsay';
+import Filter from 'bad-words';
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import Filter from 'bad-words';
 
 export const cowsay: CommandDefinition = {
     name: ['cowsay', 'cs'],
     description: 'Emulates the famous UNIX program `cowsay`.',
     category: CommandCategory.MEMES,
-    executor: async (msg) => {
-        
+    executor: (msg) => {
         const filter = new Filter();
         const text = msg.content.replace(/\.(cowsay|cs)\s*/, '').replace(/`/g, '');
 
         if (filter.isProfane(msg.content.trim())) {
-            await msg.channel.send('Watch your tongue!') 
-            return;
+            return msg.channel.send('Watch your tongue!');
         }
-
-
-
 
         if (text) {
             return msg.channel.send(`\`\`\`\n${say(({ text }))}\n\`\`\``);
