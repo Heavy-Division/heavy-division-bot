b26053c40b745cf609a25e2f09d0a21ff385b046
nakajimayoshi <Yoshi@hdsimulations.com>
2025-08-27 20:12:45 +0900

    add AVWX api types

---
+++ src/lib/schemas/avwx/station.ts
@@ -0,0 +1,27 @@
+export type Station = {
+    city:         string;
+    country:      string;
+    elevation_ft: number;
+    elevation_m:  number;
+    gps:          string;
+    iata:         string;
+    icao:         string;
+    latitude:     number;
+    local:        string;
+    longitude:    number;
+    name:         string;
+    note:         string;
+    reporting:    boolean;
+    runways:      Runway[];
+    state:        string;
+    type:         string;
+    website:      string;
+    wiki:         string;
+}
+
+export type Runway = {
+    length_ft: number;
+    width_ft:  number;
+    ident1:    string;
+    ident2:    string;
+}
